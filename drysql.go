579c1c670943aea3fda191e3516b0c538ea58fb6
vladcomp <bmitsakos@gmail.com>
2022-10-27 11:53:14 -0600

    Add custom ORM support via UpdateTableRowFromStruct

--- drysql.go
+++ drysql.go
@@ -1,7 +1,11 @@
 package drysql
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"database/sql"
+	"database/sql/driver"
 )
 
 type SqlInterface interface {
@@ -91,4 +95,62 @@ func (drysql DrySql) QueryWithoutPrepare(query string, scanner func(rows *sql.Ro
 	}
 
 	return rows.Err()
+}
+
+// UpdateTableRowFromStruct
+/*updates a specified mysql table and row with fields from a struct.   Use the `db:"column_name"` to tag struct fields with column name.  Only the non-nil values from tagged fields in the struct will be updated.
+
+	userUpdate := struct{
+		UserID `db:"user_id"`
+		FirstName *string `db:"first_name"`
+		LastName *string `db:"last_name"`
+	}
+
+	err = drysql.UpdateTableRowFromStruct("my_users", "user_id", userUpdate)
+
+*/
+
+func (drysql DrySql) UpdateTableRowFromStruct(tableName string, rowIdentifierKey string, updateStruct interface{}) (err error) {
+
+	var columnsToUpdate string
+	var inputs []interface{}
+	var rowIdentifierValue interface{}
+	t := reflect.TypeOf(updateStruct)
+	v := reflect.ValueOf(updateStruct)
+
+	// Iterate over all available fields and read the tag value
+	for i := 0; i < t.NumField(); i++ {
+		columnValue, err := driver.DefaultParameterConverter.ConvertValue(v.Field(i).Interface())
+		if err != nil {
+			return err
+		}
+		if columnValue != nil {
+			// Get the field, returns https://golang.org/pkg/reflect/#StructField
+			field := t.Field(i)
+			columnKey := field.Tag.Get("db")
+			if columnKey != "" {
+				if strings.EqualFold(columnKey, rowIdentifierKey) {
+					rowIdentifierValue = columnValue
+				} else {
+					if len(columnsToUpdate) != 0 {
+						columnsToUpdate += ", "
+					}
+					columnsToUpdate += columnKey + " = ?"
+					inputs = append(inputs, columnValue)
+				}
+			}
+		}
+	}
+
+	if len(inputs) == 0{
+		return fmt.Errorf("drysql: no fields to update")
+	}
+
+	inputs = append(inputs, rowIdentifierValue)
+
+	query := "UPDATE " + tableName + " SET " + columnsToUpdate + " WHERE " + rowIdentifierKey + " = ?"
+
+	_, err = drysql.PreparedExec(query, inputs)
+
+	return err
 }
